cffef3ece828af624caec94e4c9d56c3530a14c6
Rakesh Ramamurthy <rramamur@visa.com>
2020-10-22 16:11:09 +0530

    Moved CoreDataFeedStore to its own file.

---
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataFeedStore.swift
+//  FeedStoreChallenge
+//
+//  Created by Rakesh Ramamurthy on 22/10/20.
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+}
