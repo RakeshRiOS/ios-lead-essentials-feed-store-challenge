f2b85b0f6da62036ce98a9736e4c9ad5308c4112
Rakesh Ramamurthy <rramamur@visa.com>
2020-10-22 17:19:33 +0530

    Adding memory leaks tracker

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -98,10 +98,16 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 
         let storeURL = URL(fileURLWithPath: "/dev/null")
         let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        trackForMemoryLeaks(sut)
 
         return sut
 	}
 	
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
 }
 
 //  ***********************
